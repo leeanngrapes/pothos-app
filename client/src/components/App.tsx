46e2b1e348952904ecec18fed7a07892ff7d8960
LeeAnn Grapes <leeann.grapes@gmail.com>
2020-11-30 18:51:47 -0700

    Moved App.tsx into components folder

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Welcome to POTHOS</Text>
+      <Text style={styles.heading}>Welcome to POTHOSSS</Text>
       <StatusBar style="auto" />
     </View>
   );
